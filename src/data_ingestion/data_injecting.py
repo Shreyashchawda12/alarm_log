9a7a2ccf4737025e9c931d4bed4dd80c2ec91372
shreyashchawda12 <shreyashchawda12@gmail.com>
2025-01-07 21:55:10 +0530

    update

--- src/data_ingestion/data_injecting.py
+++ src/data_ingestion/data_injecting.py
@@ -21,7 +21,7 @@ def setup_driver():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
+    chrome_options.binary_location = "/usr/bin/chromium"
     service = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=service, options=chrome_options)
 
