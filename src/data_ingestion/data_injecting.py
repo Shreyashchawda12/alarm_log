e011234ead21e59a13e08870044fbecc7239b0e6
shreyashchawda12 <shreyashchawda12@gmail.com>
2025-01-07 21:41:25 +0530

    update

--- src/data_ingestion/data_injecting.py
+++ src/data_ingestion/data_injecting.py
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # Function to initialize the driver
 def setup_driver():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")  # Uncomment to run in headless mode
+    chrome_options.add_argument("--headless")  # Uncomment to run in headless mode
     service = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=service, options=chrome_options)
 
